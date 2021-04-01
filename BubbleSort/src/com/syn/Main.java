bd7bf57454ff2ad8d4b9108a71b303b8f890300b
sylvianikol <silviaadvert@gmail.com>
2021-04-01 13:10:53 +0300

    refactoring

--- BubbleSort/src/com/syn/Main.java
+++ BubbleSort/src/com/syn/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[] array = { 20, 35, -15, 7, 55, 1, -22 };
+        int[] arr = { 5, 9, 3, 9, 8, 4 };
 
         for (int i = 0; i < array.length; i++) {
 
