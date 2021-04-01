7ac52eeecae4f6587428804f35942db1f7e5e84e
sylvianikol <silviaadvert@gmail.com>
2021-04-01 12:41:49 +0300

    solved bubbleSort

--- BubbleSort/src/com/syn/Main.java
+++ BubbleSort/src/com/syn/Main.java
@@ -3,6 +3,27 @@ package com.syn;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] array = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%d ", array[i]);
+        }
+    }
+
+    public static void swap(int[] array, int i) {
+
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
     }
 }
