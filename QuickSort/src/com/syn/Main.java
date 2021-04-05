020ecde10751f49b28e6c316f4409e7110efae6b
sylvianikol <silviaadvert@gmail.com>
2021-04-05 11:26:07 +0300

    solved quicksort

---
+++ QuickSort/src/com/syn/Main.java
@@ -0,0 +1,47 @@
+package com.syn;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = { 20, 35, -15, 7, 55, 1, -22 };
+
+        quickSort(array, 0, array.length);
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+   public static void quickSort(int[] array, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(array, start, end);
+        quickSort(array, start, pivotIndex);
+        quickSort(array, pivotIndex + 1, end);
+   }
+
+   // 20, 35, -15, 7, 55, 1, -22
+   public static int partition(int[] array, int start, int end) {
+        int pivot = array[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+
+            while (i < j && array[--j] >= pivot);
+            if (i < j) {
+                array[i] = array[j];
+            }
+
+            while (i < j && array[++i] <= pivot);
+            if (i < j) {
+                array[j] = array[i];
+            }
+        }
+
+        array[j] = pivot;
+        return j;
+   }
+}
