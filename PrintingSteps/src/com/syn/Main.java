5364873696d5644202e858facc82a29107edd78a
sylvianikol <silviaadvert@gmail.com>
2021-04-08 14:36:02 +0300

    solved steps

---
+++ PrintingSteps/src/com/syn/Main.java
@@ -0,0 +1,36 @@
+package com.syn;
+
+public class Main {
+    // --- Directions
+    // Write a function that accepts a positive number N.
+    // The function should console log a step shape with N levels using the # character.
+    // Make sure the step has spaces on the right hand side!
+    // --- Examples
+    //   steps(2)
+    //       '# '
+    //       '##'
+    //   steps(3)
+    //       '#  '
+    //       '## '
+    //       '###'
+    //   steps(4)
+    //       '#   '
+    //       '##  '
+    //       '### '
+    //       '####'
+    public static void main(String[] args) {
+
+        steps(4);
+    }
+
+    private static void steps(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            String step = i == n
+                    ? "#".repeat(i)
+                    : "#".repeat(i) + " ";
+
+            System.out.println(step);
+        }
+    }
+}
