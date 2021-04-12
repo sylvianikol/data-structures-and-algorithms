3e1e31cab061fd16a0c9f19236d0f5de74ed3eca
sylvianikol <silviaadvert@gmail.com>
2021-04-12 14:34:35 +0300

    solved find all substr

---
+++ FindAllSubstr/src/com/syn/Main.java
@@ -0,0 +1,27 @@
+package com.syn;
+
+public class Main {
+    // For example: If input is “abb”  then output should be “a”, “b”,”b”, “ab”, “bb”, “abb”
+    public static void main(String[] args) {
+
+        String input = "abbccdd";
+        findAllSubstr(input);
+    }
+
+    private static void findAllSubstr(String input) {
+
+        int step = 1;
+
+        while (step <= input.length()) {
+            for (int i = 0; i < input.length(); i++) {
+                System.out.println(input.substring(i, i + step));
+                if (i + step == input.length()) {
+                    break;
+                }
+            }
+
+            step++;
+        }
+
+    }
+}
