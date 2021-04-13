67b2d3259f46184c3a6de79765568db2df8febae
sylvianikol <silviaadvert@gmail.com>
2021-04-13 11:53:51 +0300

    solved find min in a sorted and rotated array

--- SortedAndRotatedArray/src/com/syn/Main.java
+++ SortedAndRotatedArray/src/com/syn/Main.java
@@ -1,13 +1,33 @@
 package com.syn;
 
 public class Main {
-    // find the index of n in rotated and sorted array in o(logn) time complexity.
+    // 1. find the index of N in a sorted and rotated array in o(logn) time complexity.
+    // 2. find min element in a sorted and rotated array
     public static void main(String[] args) {
 
         int n = 5;
-        int[] array = {16, 19, 21, 25, 3, 5, 8, 10};
-        System.out.println(findIndex(array, 0, array.length - 1, n));
+        int[] array = {16, 19, 21, 1, 3, 5, 8, 10};
+//        System.out.println(findIndex(array, 0, array.length - 1, n));
+        System.out.println(findMin(array, 0, array.length - 1));
     }
+
+    private static int findMin(int[] array, int start, int end) {
+
+        int mid;
+
+        while (start < end) {
+            mid = (start + end) >>> 1;
+
+            if (array[mid] > array[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return array[start];
+    }
+
 
     private static int findIndex(int[] array, int start, int end, int number) {
 
