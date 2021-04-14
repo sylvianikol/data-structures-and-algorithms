1656a05d999230ec692aa03f89fe3d1ff10ed12e
sylvianikol <silviaadvert@gmail.com>
2021-04-14 14:53:23 +0300

    solved Find all pairs in an array whose sum is equal to N

--- ClosestToZero/src/com/syn/Main.java
+++ ClosestToZero/src/com/syn/Main.java
@@ -1,5 +1,11 @@
 package com.syn;
 
+import java.security.KeyPair;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,10 +15,31 @@ public class Main {
 //        findPairClosestToZero(array);
 
         // 2. Given a sorted array and a number n, find the pair in array whose sum is closest to n
-        int number = 5;
-        int[] array = {-40, -5, 1, 3, 6, 7, 8, 20};
+//        int number = 5;
+//        int[] array = {-40, -5, 1, 3, 6, 7, 8, 20};
+//        findPairClosestToN(array, number);
 
-        findPairClosestToN(array, number);
+        // 3. Find all pairs of elements from an array whose sum is equal to given number
+        int number = 15;
+        int[] array = {-40, -5, 1, 3, 6, 7, 8, 20};
+        findPairsEqualToN(array, number);
+    }
+
+    private static void findPairsEqualToN(int[] array, int number) {
+        Map<Integer, Integer> usedPairs = new HashMap<>();
+
+        for (int i : array) {
+            for (int j : array) {
+                if (i + j == number && !exists(usedPairs, i, j)) {
+                    System.out.println(i + " and " + j);
+                    usedPairs.put(i, j);
+                }
+            }
+        }
+    }
+
+    private static boolean exists(Map<Integer, Integer> map, int i, int j) {
+        return map.containsKey(j) && map.get(j) == i;
     }
 
     private static void findPairClosestToN(int[] array, int number) {
