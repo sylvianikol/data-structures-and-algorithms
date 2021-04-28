eb633b61c0f6d63aef50d7a02d045c72dcf3d71e
sylvianikol <silviaadvert@gmail.com>
2021-04-28 10:53:46 +0300

    solved BucketSort = using ArrayList, LinkedList or Vector

--- BucketSort/src/com/syn/Main.java
+++ BucketSort/src/com/syn/Main.java
@@ -50,7 +50,8 @@ public class Main {
 
         for (int i = 0; i < buckets.length; i++) {
 //            buckets[i] = new ArrayList<>();
-            buckets[i] = new LinkedList<>();
+//            buckets[i] = new LinkedList<>();
+            buckets[i] = new Vector<>();
         }
 
         return buckets;
