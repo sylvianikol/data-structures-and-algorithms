8c7277e6c01c410383580a2237868a0ce94f3fb5
sylvianikol <silviaadvert@gmail.com>
2021-05-04 11:41:56 +0300

    implemented binary search tree - added insert()

--- BinarySearchTree/src/com/syn/Tree.java
+++ BinarySearchTree/src/com/syn/Tree.java
@@ -4,5 +4,11 @@ public class Tree {
 
     private Node root;
 
-
+    public void insert(int value) {
+        if (root == null) {
+            root = new Node(value);
+        } else {
+            root.insert(value);
+        }
+    }
 }
