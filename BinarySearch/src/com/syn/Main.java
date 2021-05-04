2e5b472cb429bda9a5cbaddea78790211ac9a37a
sylvianikol <silviaadvert@gmail.com>
2021-05-04 10:19:18 +0300

    solved binary search - iterative, recursive

--- BinarySearch/src/com/syn/Main.java
+++ BinarySearch/src/com/syn/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
         int[] array = { -22, -15, 1, 7, 20, 35, 55 };
 
-        int index = recursiveFind(array, 20, 0, array.length - 1);
+//        int index = recursiveFind(array, 20, 0, array.length - 1);
+        int index = iterativeFind(array, 55);
 
         System.out.println(index);
     }
@@ -33,4 +34,32 @@ public class Main {
 
         return -1;
     }
+
+    private static int iterativeFind(int[] array, int num) {
+
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start != end) {
+
+            int middle = (start + end) / 2;
+
+            if (array[middle] == num) {
+                return middle;
+            }
+
+            if (array[middle] > num) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+
+        if (array[start] == num) {
+            return start;
+        }
+
+        return -1;
+    }
+
 }
